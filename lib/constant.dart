9dd34f0cda1b271f45876ce9f77cb7440897e3f3
internetova <internetova@yandex.ru>
2020-12-05 18:40:30 +0300

    Text и TextStyle

---
+++ lib/constant.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const kColorBackground = Color(0xffF5F5F5);
+
+const kColorWhiteGreen = Color(0xff4CAF50);
+const kColorWhiteYellow = Color(0xffFCDD3D);
+const kColorWhiteError = Color(0xffEF4343);
+const kColorWhiteWhite = Colors.white;
+const kColorWhiteMain = Color(0xff252849);
+const kColorWhiteSecondary = Color(0xff3B3E5B);
+const kColorWhiteSecondary2 = Color(0xff7C7E92);
+const kColorWhiteInactiveBlack = Color.fromRGBO(124, 126, 146, 0.56);
+
+const kColorBlackGreen = Color(0xff6ADA6F);
+const kColorBlackYellow = Color(0xffFFE769);
+const kColorBlackError = Color(0xffCF2A2A);
+const kColorBlackWhite = Colors.white;
+const kColorBlackDark = Color(0xff1A1A20);
+const kColorBlackMain = Color(0xff21222C);
+const kColorBlackSecondary = Color(0xff3B3E5B);
+const kColorBlackSecondary2 = Color(0xff7C7E92);
+const kColorBlackInactiveBlack = Color.fromRGBO(124, 126, 146, 0.56);
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../constant.dart';
 
 class SightListScreen extends StatefulWidget {
   @override
@@ -9,8 +10,27 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kColorWhiteWhite,
+      appBar: AppBar(
+        title: Text(
+          'Список\nинтересных мест',
+          style: TextStyle(
+            color: kColorWhiteSecondary,
+            fontFamily: 'Roboto',
+            fontSize: 32.0,
+            height: 1.125,
+            fontWeight: FontWeight.w700,
+          ),
+          textAlign: TextAlign.left,
+        ),
+        toolbarHeight: 112.0,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: Center(
-        child: Text('Hello!'),
+        child: Text(
+          'Hello !',
+        ),
       ),
     );
   }
