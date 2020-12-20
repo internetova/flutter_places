b88e54bca5e9c4e1f34686ba1e5fac4dd9af8224
internetova <internetova@yandex.ru>
2020-12-20 10:41:50 +0300

    gitignore res

---
+++ lib/ui/screen/res/text_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+/// названия из фигмы
+/// LargeTitle 32
+/// Title 24
+/// Subtitle 18
+/// Text 16
+/// Small 14
+
+/// Стили текстов
+
+const textLargeTitle32 = TextStyle(
+  fontSize: 32.0,
+  height: 1.125,
+  fontWeight: FontWeight.w700,
+);
+
+const textTitle24 = TextStyle(
+  fontSize: 24.0,
+  height: 1.2,
+  fontWeight: FontWeight.w700,
+);
+
+const textSubtitle18 = TextStyle(
+  fontSize: 18.0,
+  height: 1.33,
+  fontWeight: FontWeight.w500,
+);
+
+const textText16 = TextStyle(
+  fontSize: 16.0,
+  height: 1.25,
+  fontWeight: FontWeight.w500,
+);
+
+const textSmall14 = TextStyle(
+  fontSize: 14.0,
+  height: 1.29,
+  fontWeight: FontWeight.w400,
+);
+
+final textSmall14Bold = textSmall14.copyWith(
+  fontWeight: FontWeight.w700,
+);
+
+final textButton = textSmall14Bold.copyWith(
+  letterSpacing: 0.3,
+);
---
+++ lib/ui/screen/res/themes.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:places/ui/screen/res/colors.dart';
+import 'package:places/ui/screen/res/text_styles.dart';
+
+class AppTheme {
+  AppTheme();
+
+  static ThemeData buildTheme() {
+    final ThemeData base = ThemeData.light();
+
+    return base.copyWith(
+      brightness: Brightness.light,
+      primaryColor: lightPrimaryColor,
+      primaryColorLight: lightPrimaryColorLight,
+      primaryColorDark: lightPrimaryColorLight,
+      accentColor: lightAccentColor,
+      backgroundColor: lightScaffoldBackgroundColor,
+      scaffoldBackgroundColor: lightScaffoldBackgroundColor,
+      canvasColor: lightPrimaryColor,
+      primaryTextTheme: _buildPrimaryTextTheme(base.primaryTextTheme),
+      textTheme: _buildTextTheme(base.textTheme),
+      colorScheme: base.colorScheme.copyWith(
+        background: lightBackgroundColor,
+        secondary: lightSecondaryColor,
+        secondaryVariant: lightSecondaryVariant,
+        onPrimary: lightPrimaryColor,
+      ),
+      appBarTheme: base.appBarTheme.copyWith(
+        color: lightPrimaryColor,
+        textTheme: base.textTheme.copyWith(
+          headline6: textSubtitle18.copyWith(color: lightPrimaryColorDark),
+        ),
+        elevation: 0,
+      ),
+      tabBarTheme: base.tabBarTheme.copyWith(
+        indicator: BoxDecoration(
+          color: lightSecondaryColor,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        labelStyle: textSmall14Bold,
+        labelColor: lightPrimaryColor,
+        unselectedLabelColor: lightBackgroundColor,
+        unselectedLabelStyle: textSmall14Bold,
+      ),
+      bottomNavigationBarTheme: base.bottomNavigationBarTheme.copyWith(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: lightPrimaryColor,
+        selectedItemColor: lightPrimaryColorDark,
+        unselectedItemColor: lightSecondaryColor,
+      ),
+      buttonTheme: base.buttonTheme.copyWith(
+        buttonColor: lightButtonColor,
+        textTheme: ButtonTextTheme.primary,
+      ),
+      iconTheme: base.iconTheme.copyWith(
+        color: lightIconColor,
+        size: 24,
+      ),
+    );
+  }
+
+  static _buildTextTheme(TextTheme base) {
+    return base.copyWith(
+      headline6: textSubtitle18.copyWith(color: lightPrimaryColorDark),
+      headline5: textText16.copyWith(color: lightSecondaryColor),
+      headline4: textTitle24.copyWith(color: lightSecondaryColor),
+      headline3: textLargeTitle32.copyWith(color: lightPrimaryColorDark),
+      subtitle1: textSmall14Bold.copyWith(color: lightSecondaryColor),
+      subtitle2: textSmall14Bold.copyWith(color: lightPrimaryColor),
+      bodyText1: textSmall14.copyWith(color: lightSecondaryColor),
+      bodyText2: textSmall14.copyWith(color: lightSecondaryVariant),
+      button: textButton,
+    );
+  }
+
+  static _buildPrimaryTextTheme(TextTheme base) {
+    return base.copyWith(
+      headline6: textSubtitle18.copyWith(color: lightBackgroundColor),
+      bodyText1: textSmall14.copyWith(color: lightAccentColor),
+      bodyText2: textSmall14.copyWith(color: lightBackgroundColor),
+    );
+  }
+
+  ///DARK
+  static ThemeData buildThemeDark() {
+    final ThemeData base = ThemeData.dark();
+
+    return base.copyWith(
+      brightness: Brightness.dark,
+      primaryColor: darkPrimaryColor,
+      primaryColorLight: darkPrimaryColorDark,
+      primaryColorDark: darkPrimaryColorDark,
+      accentColor: darkAccentColor,
+      backgroundColor: darkScaffoldBackgroundColor,
+      scaffoldBackgroundColor: darkScaffoldBackgroundColor,
+      canvasColor: darkPrimaryColor,
+      primaryTextTheme: _buildPrimaryTextThemeDark(base.primaryTextTheme),
+      textTheme: _buildTextThemeDark(base.textTheme),
+      colorScheme: base.colorScheme.copyWith(
+        background: darkBackgroundColor,
+        secondary: darkSecondaryColor,
+        secondaryVariant: darkSecondaryVariant,
+        onPrimary: colorWhite,
+      ),
+      appBarTheme: base.appBarTheme.copyWith(
+        color: darkPrimaryColor,
+        textTheme: base.textTheme.copyWith(
+          headline6: textSubtitle18.copyWith(color: colorWhite),
+        ),
+        elevation: 0,
+      ),
+      tabBarTheme: base.tabBarTheme.copyWith(
+        indicator: BoxDecoration(
+          color: colorWhite,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        labelStyle: textSmall14Bold,
+        labelColor: darkSecondaryColor,
+        unselectedLabelColor: darkSecondaryVariant,
+        unselectedLabelStyle: textSmall14Bold,
+      ),
+      bottomNavigationBarTheme: base.bottomNavigationBarTheme.copyWith(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: darkPrimaryColor,
+        selectedItemColor: colorWhite,
+        unselectedItemColor: colorWhite,
+      ),
+      buttonTheme: base.buttonTheme.copyWith(
+        buttonColor: darkButtonColor,
+        textTheme: ButtonTextTheme.primary,
+      ),
+      iconTheme: base.iconTheme.copyWith(
+        color: darkIconColor,
+        size: 24,
+      ),
+    );
+  }
+
+  static _buildTextThemeDark(TextTheme base) {
+    return base.copyWith(
+      headline6: textSubtitle18.copyWith(color: colorWhite),
+      headline5: textText16.copyWith(color: colorWhite),
+      headline4: textTitle24.copyWith(color: colorWhite),
+      headline3: textLargeTitle32.copyWith(color: colorWhite),
+      subtitle1: textSmall14Bold.copyWith(color: darkSecondaryVariant),
+      subtitle2: textSmall14Bold.copyWith(color: colorWhite),
+      bodyText1: textSmall14.copyWith(color: colorWhite),
+      bodyText2: textSmall14.copyWith(color: darkBackgroundColor),
+      button: textButton,
+    );
+  }
+
+  static _buildPrimaryTextThemeDark(TextTheme base) {
+    return base.copyWith(
+      headline6: textSubtitle18.copyWith(color: darkBackgroundColor),
+      bodyText1: textSmall14.copyWith(color: darkAccentColor),
+      bodyText2: textSmall14.copyWith(color: darkBackgroundColor),
+    );
+  }
+}
