2b6ef188eaf43297a283ad0ff18c1146cb826ea9
internetova <internetova@yandex.ru>
2021-01-20 15:41:48 +0300

    сортировка карточек в избранном перетаскиванием

--- lib/ui/screen/widgets/sight_card_visiting.dart
+++ lib/ui/screen/widgets/sight_card_visiting.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:places/data.dart';
+import 'package:places/domain/sight.dart';
 import 'package:places/ui/screen/components/icon_svg.dart';
 import 'package:places/ui/screen/res/assets.dart';
 import 'package:places/ui/screen/res/sizes.dart';
 import 'package:places/ui/screen/res/strings.dart';
+import 'package:places/ui/screen/visiting_screen.dart';
+import 'package:places/ui/screen/widgets/sight_card.dart';
 import 'package:places/ui/screen/res/themes.dart';
+
+/// карточка для избранного
+class SightCardVisiting extends StatelessWidget {
+  final Sight card;
+  final WhereShowCard whereShowCard;
+
+  const SightCardVisiting({
+    Key key,
+    @required this.card,
+    @required this.whereShowCard,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 24.0),
+      child: Stack(
+        children: [
+          DismissBackgroundCard(),
+          Dismissible(
+            key: UniqueKey(),
+            onDismissed: (direction) {
+              favoritesSight.removeWhere((element) => element.id == card.id);
+              VisitingScreen.of(context).updateState();
+            },
+            direction: DismissDirection.endToStart,
+            child: SightCard(
+              card: card,
+              whereShowCard: whereShowCard,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 /// фон при смахивании картинки на экране добавления нового места
 class DismissBackgroundCard extends StatelessWidget {
